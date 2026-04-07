fceb532c262f751db9a88e83df3d341885eb3da3
TypeClecio <devclecio@gmail.com>
2026-04-07 19:36:48 -0300

    Adicionando Aliases ao projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@data': fileURLToPath(new URL('./src/data', import.meta.url)),
+      '@interfaces': fileURLToPath(new URL('./src/interfaces', import.meta.url)),
+    },
+  },
   server: {
     host: true,
   },
