24505e9d45b5f8891547f745c6f0f8feb4aa0ea3
TypeClecio <devclecio@gmail.com>
2026-04-06 22:10:09 -0300

    Deixando a estrutura da página inicial padrão

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,16 @@
+import './styles.scss'
+
 function Home() {
   return (
-    <main className="app">
-      <h1>Template React + Vite</h1>
-      <p>
+    <main className="home">
+      <span className="home__badge">Exemplo</span>
+      <h1 className="home__title">React + Vite + TypeScript</h1>
+      <p className="home__text">
         Edite <code>src/pages/Home/index.tsx</code> e salve para ver as mudancas.
       </p>
+      <div className="home__card">
+        Este bloco serve como exemplo simples de estilos locais.
+      </div>
     </main>
   )
 }
